5d74a5585a76b12c3a69e4937a398be1faf818e6
Doug <dougmdev@outlook.com>
2022-06-16 19:45:33 +0100

    initialise mongo setup and moved the connection process

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware to display requsts
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.use(workoutRoutes);
-
-app.listen(process.env.PORT, () => {
-  console.log(`Backend Active on Port ${process.env.PORT}`);
+//basic routes
+app.use("/api/workouts", workoutRoutes);
+//error route
+app.get("*", (req, res) => {
+  res.json({ message: "Sorry This Page Cannot Be Found" });
 });
+
+//Connecting to the DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the Database");
+    //Listening on the App once connected to the Database
+    app.listen(process.env.PORT, () => {
+      console.log(`Backend Active on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
