c75fc5978b4c23b4da77951f9762dbcc993015c7
Doug <dougmdev@outlook.com>
2022-06-16 19:03:12 +0100

    middleware pathing initialised

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,16 @@ const express = require("express");
 
 const app = express();
 
+// middleware to display requsts
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the App" });
 });
 
-app.listen(process.env.PORT, (req, res) => {
+app.listen(process.env.PORT, () => {
   console.log(`Backend Active on Port ${process.env.PORT}`);
 });
